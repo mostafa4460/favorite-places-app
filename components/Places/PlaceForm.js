b0c6fa261920a208061989831d7f7134ba2c21c8
mostafa4460 <mostafa.nassr88@gmail.com>
2023-05-05 01:28:39 -0400

    create PlaceForm component

--- components/Places/PlaceForm.js
+++ components/Places/PlaceForm.js
@@ -1,11 +1,44 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { ScrollView, StyleSheet, Text, TextInput } from "react-native";
+import { COLORS } from "../../constants/colors";
+import ImagePicker from "./ImagePicker";
 
 const PlaceForm = () => {
+  const [title, setTitle] = useState("");
+
   return (
-    <View>
-      <Text>The place form</Text>
-    </View>
+    <ScrollView style={styles.form}>
+      <Text style={styles.label}>Title</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={(value) => setTitle(value)}
+        value={title}
+      />
+
+      <ImagePicker />
+    </ScrollView>
   );
 };
 
 export default PlaceForm;
+
+const styles = StyleSheet.create({
+  form: {
+    flex: 1,
+    padding: 24,
+  },
+  label: {
+    fontWeight: "bold",
+    marginBottom: 4,
+    color: COLORS.primary500,
+  },
+  input: {
+    marginVertical: 8,
+    paddingHorizontal: 4,
+    paddingVertical: 8,
+    fontSize: 16,
+    borderBottomColor: COLORS.primary700,
+    borderBottomWidth: 2,
+    backgroundColor: COLORS.primary100,
+  },
+});

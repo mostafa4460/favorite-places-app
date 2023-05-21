de1c73d1f85b8ba30d6be20641a90d8e76ba4022
mostafa4460 <mostafa.nassr88@gmail.com>
2023-05-21 17:27:12 -0400

    add new Map screen

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import AllPlaces from "./screens/AllPlaces";
 import AddPlace from "./screens/AddPlace";
 import IconButton from "./components/UI/IconButton";
 import { COLORS } from "./constants/colors";
+import Map from "./screens/Map";
 
 const Stack = createNativeStackNavigator();
 
@@ -41,6 +42,7 @@ export default function App() {
             component={AddPlace}
             options={{ title: "Add New Place" }}
           />
+          <Stack.Screen name="Map" component={Map} />
         </Stack.Navigator>
       </NavigationContainer>
     </>
